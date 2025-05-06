2834fbc55ddbfcc39efdf3c4c393999322f32711
阿男 <l.weinan@gmail.com>
2025-05-07 01:00:34 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/my-org/my-repo
+
+go 1.18 
---
+++ math_test.go
@@ -0,0 +1,11 @@
+package math
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	result := Add(2, 3)
+	expected := 5
+	if result != expected {
+		t.Errorf("Add(2, 3) = %d; want %d", result, expected)
+	}
+}
